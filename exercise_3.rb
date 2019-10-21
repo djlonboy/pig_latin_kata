87980893388b81192eb6f1724e29e19d2be6ce85
djlonboy <djlonboy@gmail.com>
2019-10-21 15:22:00 +0100

    completed exercise_3

--- exercise_3.rb
+++ exercise_3.rb
@@ -5,7 +5,39 @@
 #      qat   -> atqay
 
 def pig_latin(word)
+  if word.slice(0) == "a" ||
+     word.slice(0) == "e" ||
+     word.slice(0) == "i" ||
+     word.slice(0) == "o" ||
+     word.slice(0) == "u"
+     word += "way"
 
+  elsif word.slice(0..1) == "qu"
+        word = word.slice(2..word.length) + word.slice(0..1) + "ay"
+
+  elsif word.slice(1) == "a" ||
+        word.slice(1) == "e" ||
+        word.slice(1) == "i" ||
+        word.slice(1) == "o" ||
+        word.slice(1) == "u"
+        word = word.slice(1..word.length) + word.slice(0) + "ay"
+
+  elsif word.slice(2) == "a" ||
+        word.slice(2) == "e" ||
+        word.slice(2) == "i" ||
+        word.slice(2) == "o" ||
+        word.slice(2) == "u"
+        word = word.slice(2..word.length) + word.slice(0..1) + "ay"
+
+  elsif word.slice(3) == "a" ||
+        word.slice(3) == "e" ||
+        word.slice(3) == "i" ||
+        word.slice(3) == "o" ||
+        word.slice(3) == "u"
+        word = word.slice(3..word.length) + word.slice(0..2) + "ay"
+
+  end
+return word
 end
 
 ## Tests:
